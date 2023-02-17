8675cd06d428afbad860ad24488350f060317f36
Robin Weser <contact@weser.io>
2023-02-17 21:50:58 +0100

    fix build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import useLayer from './useLayer'
-import useLayerWithSizeConstraints from './useLayerWithMaxWidth'
+import useLayerWithSizeConstraints from './useLayerWithSizeConstraints'
 import useLayerCount from './useLayerCount'
 import LayerContextProvider from './LayerContextProvider'
 
