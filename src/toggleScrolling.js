bccca15f0a081043261b8d6cba8599d5e967cb60
Robin Weser <contact@weser.io>
2023-03-28 18:24:13 +0200

    final fixes

--- src/toggleScrolling.js
+++ src/toggleScrolling.js
@@ -5,6 +5,7 @@ function blockScrolling(scrollElement) {
 
   scrollElement.style.overflow = 'hidden'
   scrollElement.style.position = 'fixed'
+  scrollElement.style.width = '100%'
   scrollElement.style.top = -scrollTop + 'px'
 }
 
@@ -12,6 +13,8 @@ function enableScrolling(scrollElement) {
   scrollElement.style.removeProperty('position')
   scrollElement.style.removeProperty('overflow')
   scrollElement.style.removeProperty('top')
+  scrollElement.style.removeProperty('width')
+
   window.scrollTo(0, scrollTop)
 }
 
